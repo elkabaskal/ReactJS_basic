beb3794ae20794826b626f80b0290c88315462ad
elkabaskal <audiopt@list.ru>
2022-03-07 21:15:49 +0500

    HW for lees5
    Пакеты redux, react-redux установлены.
    redux devtools установлен.
    Чекбокс перенесен из todo урока  в профиль, но опять не рендерится на страничке profile

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,4 +1,6 @@
-import { Switch, Route } from 'react-router-dom';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { Home } from "./routes/Home";
 import { Projects } from "./routes/Projects";
 import { Tasks } from "./routes/Tasks";
@@ -6,25 +8,31 @@ import { Task } from "./routes/Task";
 import { Header } from "./components/Header";
 import { getChildrenAndRenderPropsLink, getHomeLink, getProjectsLink, getTaskLink, getTasksLink } from "./navigation";
 import { ChildrenAndRenderProps } from "./routes/Profile";
+//import { store } from './store';
 
 
 function App() {
 
     return (
-        <div>
-            <Header />
+        <div><Header />
+
             <Switch>
-                <Route exact path={getHomeLink()} component={Home} />
-                <Route exact path={getChildrenAndRenderPropsLink()} component={ChildrenAndRenderProps} />
+                <Route exact path={getHomeLink()}
+                    component={Home} />
+                <Route exact path={getChildrenAndRenderPropsLink()}
+                    component={ChildrenAndRenderProps} />
                 <Route path={getProjectsLink()}>
                     <Projects>
                         <Switch>
-                            <Route path={getTaskLink()} component={Task} />
-                            <Route path={getTasksLink()} component={Tasks} />
+                            <Route path={getTaskLink()}
+                                component={Task} />
+                            <Route path={getTasksLink()}
+                                component={Tasks} />
                         </Switch>
                     </Projects>
                 </Route>
             </Switch>
+
         </div>
     );
 }
