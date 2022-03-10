7a308ed0807ddf10914465dfe04e084b17276a6b
elkabaskal <audiopt@list.ru>
2022-03-10 12:17:16 +0500

    HW less6 is not ready
    Перенес код с урока для отображения на странице профиля, для начала, но страница не рендерится.

---
+++ myapp/src/store/index.jsx
@@ -0,0 +1,8 @@
+import { createStore, combineReducers } from 'redux';
+import { todoReducer } from './todo/reducer';
+import { projectsReducer } from "./projects/reducer";
+
+export const store = createStore(combineReducers({
+    todo: todoReducer,
+    project: projectsReducer,
+}), window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__());
